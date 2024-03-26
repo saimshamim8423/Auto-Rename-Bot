d3d2340d0d63f15f2d3733815f6c8ffe856f77dc
animeotaku1091 <154030645+animeotaku1091@users.noreply.github.com>
2024-03-26 20:49:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ class Txt(object):
     
 ➻ Use /tutorial Command To Know How To Use Me.
     
-<b>Bot Is Made By @Madflix_Bots</b>"""
+<b>Bot Is Made By @teamotaku109</b>"""
     
     FILE_NAME_TXT = """<b><u>SETUP AUTO RENAME FORMAT</u></b>
 
