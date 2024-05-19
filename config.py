706af40666f10da5441e24396291a435ef592c4e
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-05-19 11:58:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ class Txt(object):
     
 ➻ Use /tutorial Command To Know How To Use Me.
     
-<b>Bot Is Made By @teamotaku109</b>"""
+<b>Bot Is Made By @Beesmileee</b>"""
     
     FILE_NAME_TXT = """<b><u>SETUP AUTO RENAME FORMAT</u></b>
 
@@ -44,18 +44,18 @@ Use These Keywords To Setup Custom File Name
 ✓ episode :- To Replace Episode Number
 ✓ quality :- To Replace Video Resolution
 
-<b>➻ Example :</b> <code> /autorename Naruto Shippuden S02 - EPepisode - quality  [Dual Audio] - @Madflix_Bots </code>
+<b>➻ Example :</b> <code> /autorename Naruto Shippuden S02 - EPepisode - quality  [Dual Audio] - @Beesmileee </code>
 
 <b>➻ Your Current Auto Rename Format :</b> <code>{format_template}</code> """
     
-    ABOUT_TXT = f"""<b>🤖 My Name :</b> <a href='https://t.me/teamotaku109'>Auto Rename Bot ⚡</a>
+    ABOUT_TXT = f"""<b>🤖 My Name :</b> <a href='https://t.me/Anity_managementbots'>Auto Rename Bot ⚡</a>
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
 <b>📢 Channel :</b> <a href='https://t.me/Anity_managementbots'>Anity Bots</a>
 <b>🧑‍💻 Developer :</b> <a href='https://t.me/Anity_managementbots'>Anity Bots</a>
     
-<b>♻️ Bot Made By :</b> @teamotaku109"""
+<b>♻️ Bot Made By :</b> @Beesmileee"""
 
     
     THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
@@ -82,7 +82,7 @@ Use These Keywords To Setup Custom File Name
     
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
     
-<b>🛍 UPI ID:</b> <code>teamkajdicial@axl</code> """
+<b>🛍 UPI ID:</b> <code>@axl</code> """
     
     HELP_TXT = """<b>Hey</b> {}
     
